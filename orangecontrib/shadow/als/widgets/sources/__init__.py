997b75841dde20f0d093226f0c808e42c21c3610
Luca Rebuffi <luca.rebuffi@elettra.eu>
2018-02-28 11:53:38 -0800

    first commit

---
+++ orangecontrib/shadow/als/widgets/sources/__init__.py
@@ -0,0 +1,21 @@
+"""
+=========
+Associate
+=========
+
+Widgets for association rules.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "ALS Sources"
+
+DESCRIPTION = "Widgets for Shadow Sources."
+
+BACKGROUND = "#fffeb3"
+#BACKGROUND = "#A9D0F5"
+
+ICON = "icons/source.png"
+
+PRIORITY = 101
