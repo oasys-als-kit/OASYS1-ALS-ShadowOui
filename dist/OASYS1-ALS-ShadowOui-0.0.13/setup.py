e906c0425358fc2ad9e7973f1f14b80ea3d75a33
Manuel Sanchez del Rio <srio@esrf.eu>
2020-01-06 14:05:48 -0800

    update

---
+++ dist/OASYS1-ALS-ShadowOui-0.0.13/setup.py
@@ -0,0 +1,121 @@
+#! /usr/bin/env python3
+
+import imp
+import os
+import sys
+import subprocess
+
+NAME = 'OASYS1-ALS-ShadowOui'
+
+VERSION = '0.0.14'
+ISRELEASED = False
+
+DESCRIPTION = 'WIDGETS DEVELOPED FOR ALS TO EXTEND SHADOWOUI FUNCTIONALITIES'
+README_FILE = os.path.join(os.path.dirname(__file__), 'README.txt')
+LONG_DESCRIPTION = open(README_FILE).read()
+AUTHOR = 'Luca Rebuffi and Antoine Wojdyla and Manuel Sanchez del Rio'
+AUTHOR_EMAIL = 'awojdyla@lbl.gov, srio@lbl.gov'
+URL = 'https://github.com/oasys-als-kit/OASYS1-ALS-ShadowOui'
+DOWNLOAD_URL = 'https://github.com/oasys-als-kit/OASYS1-ALS-ShadowOui'
+LICENSE = 'MIT'
+
+KEYWORDS = (
+    'ray-tracing',
+    'simulator',
+    'oasys1',
+)
+
+CLASSIFIERS = (
+    'Development Status :: 4 - Beta',
+    'Environment :: X11 Applications :: Qt',
+    'Environment :: Console',
+    'Environment :: Plugins',
+    'Programming Language :: Cython',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Science/Research',
+)
+
+
+SETUP_REQUIRES = (
+                  'setuptools',
+                  )
+
+INSTALL_REQUIRES = (
+                    'setuptools',
+                    'shadow4>=0.0.12'
+                   )
+
+if len({'develop', 'release', 'bdist_egg', 'bdist_rpm', 'bdist_wininst',
+        'install_egg_info', 'build_sphinx', 'egg_info', 'easy_install',
+        'upload', 'test'}.intersection(sys.argv)) > 0:
+    import setuptools
+    extra_setuptools_args = dict(
+        zip_safe=False,  # the package can run out of an .egg file
+        include_package_data=True,
+        install_requires=INSTALL_REQUIRES
+    )
+else:
+    extra_setuptools_args = dict()
+
+from setuptools import find_packages, setup
+
+PACKAGES = find_packages(
+                         exclude = ('*.tests', '*.tests.*', 'tests.*', 'tests'),
+                         )
+
+PACKAGE_DATA = {"orangecontrib.shadow.als.widgets.sources":["icons/*.png", "icons/*.jpg"],
+                "orangecontrib.shadow.als.widgets.optical_elements":["icons/*.png", "icons/*.jpg"],
+                "orangecontrib.shadow.als.widgets.utility":["icons/*.png", "icons/*.jpg",],
+                "orangecontrib.xoppy.als.widgets.srcalc":["icons/*.png", "icons/*.jpg",],
+}
+
+NAMESPACE_PACAKGES = ["orangecontrib",
+                      "orangecontrib.shadow", "orangecontrib.shadow.als", "orangecontrib.shadow.als.widgets",
+                      "orangecontrib.xoppy", "orangecontrib.xoppy.als", "orangecontrib.xoppy.als.widgets",
+                      ]
+
+ENTRY_POINTS = {
+    'oasys.addons' : ("ALS ShadowOui = orangecontrib.shadow.als","ALS XOPPY = orangecontrib.xoppy.als" ),
+    'oasys.widgets' : (
+        "Shadow ALS Sources = orangecontrib.shadow.als.widgets.sources",
+        "Shadow ALS Optical Elements = orangecontrib.shadow.als.widgets.optical_elements",
+        "Shadow ALS Utility = orangecontrib.shadow.als.widgets.utility",
+        "XOPPY ALS SRCALC = orangecontrib.xoppy.als.widgets.srcalc",
+    ),
+}
+
+if __name__ == '__main__':
+    is_beta = False
+
+    try:
+        import PyMca5, PyQt4
+
+        is_beta = True
+    except:
+        setup(
+              name = NAME,
+              version = VERSION,
+              description = DESCRIPTION,
+              long_description = LONG_DESCRIPTION,
+              author = AUTHOR,
+              author_email = AUTHOR_EMAIL,
+              url = URL,
+              download_url = DOWNLOAD_URL,
+              license = LICENSE,
+              keywords = KEYWORDS,
+              classifiers = CLASSIFIERS,
+              packages = PACKAGES,
+              package_data = PACKAGE_DATA,
+              #          py_modules = PY_MODULES,
+              setup_requires = SETUP_REQUIRES,
+              install_requires = INSTALL_REQUIRES,
+              #extras_require = EXTRAS_REQUIRE,
+              #dependency_links = DEPENDENCY_LINKS,
+              entry_points = ENTRY_POINTS,
+              namespace_packages=NAMESPACE_PACAKGES,
+              include_package_data = True,
+              zip_safe = False,
+              )
+
+    if is_beta: raise NotImplementedError("This version of <APP NAME> doesn't work with Oasys1 beta.\nPlease install OASYS1 final release: http://www.elettra.eu/oasys.html")
